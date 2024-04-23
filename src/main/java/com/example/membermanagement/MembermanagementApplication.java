d171b81cff824b2b323c60f981700b5dc7cda200
incheons <sonincheon94@naver.com>
2024-04-23 12:22:08 +0900

    spring boot 환경설정 , mysql 세팅

---
+++ src/main/java/com/example/membermanagement/MembermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.membermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembermanagementApplication.class, args);
+	}
+
+}
